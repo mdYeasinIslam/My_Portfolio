37c08e7d93cee1f72e95e6c538d16b73b35807e5
mdYeasinIslam <hyeasinislam@gmail.com>
2025-12-06 20:35:43 +0600

    fix eslint related build time error

--- eslint.config.js
+++ eslint.config.js
@@ -3,36 +3,33 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import nextPlugin from "eslint-plugin-next";
+import nextConfig from "eslint-config-next";
 
 export default tseslint.config(
   { ignores: ["dist"] },
 
   {
-    // Next.js plugin + recommended rules
-    plugins: {
-      next: nextPlugin,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+    files: ["**/*.{ts,tsx,js,jsx}"],
 
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      nextPlugin.configs.recommended, // <-- REQUIRED
-      nextPlugin.configs["core-web-vitals"], // <-- Optional but recommended
+      nextConfig, // <-- Next.js recommended config
+      nextConfig["core-web-vitals"], // <-- Optional but recommended
     ],
-
-    files: ["**/*.{ts,tsx}"],
 
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
 
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+
     rules: {
       ...reactHooks.configs.recommended.rules,
-
       "react-refresh/only-export-components": [
         "off",
         { allowConstantExport: true },
